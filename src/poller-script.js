fb84dc7e9af4ab38dc70135cc800be6648cd99ef
Anson Wayman <Anson.Wayman@extendhealth.com>
2017-05-10 07:19:00 -0600

    Add env variable to disable this plugin
    This is necessary because we have a stage hubot and a real hubot at our
    company and we don't want the stage hubot saying anything about the RSS
    feeds.

--- src/poller-script.js
+++ src/poller-script.js
@@ -5,8 +5,10 @@ export default async function rssPoller(robot) {
   try {
     const config = await loadFile(process.env.HUBOT_RSS_CONFIG_FILE || 'hubotrssconfig.json');
 
-    JSON.parse(config).feeds.map(x => getFeed({ ...x, robot }))
-      .forEach(x => x.startFeed());
+    if (!process.env.HUBOT_RSS_FEED_DISABLE) {
+      JSON.parse(config).feeds.map(x => getFeed({ ...x, robot }))
+        .forEach(x => x.startFeed());
+    }
   } catch (err) {
     robot.logger.debug(err.message);
   }
